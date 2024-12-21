fd62af5add1d6b174322f8a4037430f28df3876d
Michal Graffy <graffymichal@gmail.com>
2024-12-21 18:36:51 +0100

    Update initial (game version 1.1.2)

--- Pack-Sources.bat
+++ Pack-Sources.bat
@@ -1,1 +1,4 @@
-.\repak.exe pack .\Changed Stalker-Evo.pak --version V11
+.\repak.exe pack .\Changed\Revo Modfiles\Deta_STALKER-EVO_Revo.pak --version V11
+.\repak.exe pack .\Changed\Consumables Modfiles\Deta_STALKER-EVO_Consumables.pak --version V11
+.\repak.exe pack .\Changed\CoreVariables Modfiles\Deta_STALKER-EVO_CoreVariables.pak --version V11
+.\repak.exe pack .\Changed\Weapons Modfiles\Deta_STALKER-EVO_Weapons.pak --version V11
